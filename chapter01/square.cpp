89f86091dc4357541abd9cf894716b0433654a61
weekend27 <weekend27@163.com>
2015-08-12 00:12:13 +0800

    chapter01

---
+++ chapter01/square.cpp
@@ -0,0 +1,20 @@
+
+//Calculate the square of a number
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	double n;
+
+	//Prompt and input value of n.
+
+	cout << "Input a number and press ENTER: ";
+	cin >> n;
+
+	//Calculate and output the square.
+
+	cout << "The square is: " << n * n << endl;
+
+	return 0;
+}
